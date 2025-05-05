ee06c6b54ca64d58054346157be94491fe511133
Your Name <your.email@example.com>
2025-05-05 10:24:41 -0800

    lets go

--- jacobPim-finalOne.py
+++ jacobPim-finalOne.py
@@ -4,22 +4,17 @@ import cv2
 square =np.zeros((8, 8), dtype=np.uint8)
 
 def makeCol(end1, end2):
-    
-    # gap = abs(end1-end2)
     array = []
     for i in range(8):
-        value = int((end1 * i/7) + (end2 * i/7)) 
-        print(value)
+        value = int((end2 * i/7) + (end1 * (7-i)/7))
         array.append(value)
 
     return array
 
 def makeRow(end1, end2, row):
-    gap = abs(end1-end2)
     for i in range(8):
-        value = int((end1 * i/7) + (end2 * i/7)) 
+        value = int((end2 * i/7) + (end1 * (7 - i)/7))
         square[row][i] = value
-        # square[row][i] = 255
 
 nw = 111
 ne = 186
@@ -31,7 +26,6 @@ right = makeCol(ne, se)
 
 for i in range(8):
     makeRow(left[i], right[i], i)
-
 
 cv2.namedWindow("Gradient",cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Gradient",600,600)
